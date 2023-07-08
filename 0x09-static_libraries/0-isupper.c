edd72d629452e03c8385b639be6f182f82375b96
Sellesta <brilliantpenman@gmai.com>
2023-07-08 14:38:04 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - Checks for uppercase characters.
+* @c: Character to be checked.
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
