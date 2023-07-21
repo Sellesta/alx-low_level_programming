33b7af7e67537585263774764840c5e3f4982377
Sellesta <brilliantpenman@gmai.com>
2023-07-21 11:25:57 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,93 +1,51 @@
 #include "variadic_functions.h"
-#include <string.h>
 
 /**
- * print_all - Function entry
- * Description: A function that prints anything
- * @format: a list of types of arguments passed to the function
- * @...: list of arguments
- * Return: See nothing!
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	char *sep = "";
-	va_list args;
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	converter toCheck[] = {
-		{"s", _printString},
-		{"i", _printInt},
-		{"f", _printFloat},
-		{"c", _printChar},
-	};
-
-	va_start(args, format);
-
-	while (format != NULL && format[i / 4] != '\0')
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		unsigned int j = i % 4;
-
-		if ((*(toCheck + j)).type[0] == format[i / 4])
+		j = 0;
+		while (t_arg[j])
 		{
-			printf("%s", sep);
-			toCheck[j].func(args);
-			sep = ", ";
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
 		}
-		i++;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-	putchar('\n');
-
-	va_end(args);
-}
-
-
-/**
- * _printString - Function entry
- * Description: A function that print the string type
- * @myList: va_list passed to the function
- * Return: See nothing!
- */
-void _printString(va_list myList)
-{
-	char *string = va_arg(myList, char *);
-
-	if (string == NULL)
-		string = "(nil)";
-
-	printf("%s", string);
-}
-
-
-/**
- * _printFloat - Function entry
- * Description: A function that print the float type
- * @myList: va_list passed to the function
- * Return: See nothing!
- */
-void _printFloat(va_list myList)
-{
-	printf("%f", va_arg(myList, double));
-}
-
-/**
- * _printInt - Function entry
- * Description: A function that print the int type
- * @myList: va_list passed to the function
- * Return: See nothing!
- */
-void _printInt(va_list myList)
-{
-	printf("%d", va_arg(myList, int));
-}
-
-
-/**
- * _printChar - Function entry
- * Description: A function that print the char type
- * @myList: va_list passed to the function
- * Return: See nothing!
- */
-void _printChar(va_list myList)
-{
-	printf("%c", va_arg(myList, int));
+	printf("\n"), va_end(valist);
 }
