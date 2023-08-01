a2d73ff7ed4e3703bc5af9f5a2d71de692ed9ca2
Sellesta <brilliantpenman@gmail.com>
2023-08-01 04:23:53 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * insert_nodeint_at_index - Function entry
@@ -10,6 +10,7 @@
  * if it is not possible to add the new node at index idx
  * ...do not add the new node and return NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
